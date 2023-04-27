4200e16334ad94c7c0697da97921d6250a9b5238
liangweijie2 <liangweijie2@yy.com>
2023-04-27 20:06:55 +0800

    feat(智能识别颜色区域): 删除无用代码。

--- YYEVA/Classes/utils/YYEVARegionChecker.m
+++ YYEVA/Classes/utils/YYEVARegionChecker.m
@@ -71,7 +71,6 @@
     
     for (int i = 0; i<count; i++) {
         @autoreleasepool {
-            NSTimeInterval start = [[NSDate date] timeIntervalSince1970];
             CMSampleBufferRef sampleBufferRef = [self getNextSampleBufferRefWithStep:i == 0 ? 1 : 10];
             if (sampleBufferRef == NULL) {
                 continue;
