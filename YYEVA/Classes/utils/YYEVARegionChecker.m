a69f1e80367fddfb26dc14b6fb244cd5bedf912a
liangweijie2 <liangweijie2@yy.com>
2023-05-31 11:44:57 +0800

    feat(智能识别颜色区域): 优化智能区域检测。

--- YYEVA/Classes/utils/YYEVARegionChecker.m
+++ YYEVA/Classes/utils/YYEVARegionChecker.m
@@ -11,21 +11,21 @@
 #import <CoreMedia/CMFormatDescription.h>
 #import <Accelerate/Accelerate.h>
 
-@interface YYRGBPixelNode : NSObject
-@property (nonatomic, assign) NSUInteger r;
-@property (nonatomic, assign) NSUInteger g;
-@property (nonatomic, assign) NSUInteger b;
+@interface YYPixelNode : NSObject
 @property (nonatomic, assign) CGPoint point;
+@property (nonatomic, assign) NSUInteger y;
+@property (nonatomic, assign) NSUInteger u;
+@property (nonatomic, assign) NSUInteger v;
 @property (nonatomic, assign) BOOL isColorNode;
 @end
 
-@implementation YYRGBPixelNode
+@implementation YYPixelNode
 - (instancetype)init
 {
     if (self = [super init]) {
-        _r = 0;
-        _g = 0;
-        _b = 0;
+        _y = 0;
+        _u = 0;
+        _v = 0;
         _point = CGPointZero;
     }
     return self;
@@ -36,7 +36,6 @@
 @property (nonatomic, strong) AVURLAsset *asset;
 @property (nonatomic, strong) AVAssetReader *reader;
 @property (nonatomic, strong) AVAssetReaderTrackOutput *output;
-@property (nonatomic, strong) CIContext *ciContext;
 @end
 
 @implementation YYEVARegionChecker
@@ -93,27 +92,23 @@
     CVPixelBufferRef yuvPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer);
     CVPixelBufferLockBaseAddress(yuvPixelBuffer, 0);
     
-    CVPixelBufferRef rgbPixelBuffer = [self getRGBPixelBufferFromYUVPixelBuffer:yuvPixelBuffer];
-    CVPixelBufferLockBaseAddress(rgbPixelBuffer, 0);
+    size_t pWidth = CVPixelBufferGetWidth(yuvPixelBuffer);
+    size_t pHeight = CVPixelBufferGetHeight(yuvPixelBuffer);
 
-    size_t pWidth = CVPixelBufferGetWidth(rgbPixelBuffer);
-    size_t pHeight = CVPixelBufferGetHeight(rgbPixelBuffer);
-
-    
     //    抽取400个点
     NSUInteger sizePoint = 10;
-    NSArray<YYRGBPixelNode *> *LTNodes = [self getSquareSidePoints:sizePoint
+    NSArray<YYPixelNode *> *LTNodes = [self getSquareSidePoints:sizePoint
                                                     PointsWithRect:CGRectMake(0, 0, pWidth / 2.0, pHeight / 2.0)
-                                                     InPixelBuffer:rgbPixelBuffer]; // 左上角
-    NSArray<YYRGBPixelNode *> *RTNodes = [self getSquareSidePoints:sizePoint
+                                                     InPixelBuffer:yuvPixelBuffer]; // 左上角
+    NSArray<YYPixelNode *> *RTNodes = [self getSquareSidePoints:sizePoint
                                                     PointsWithRect:CGRectMake( pWidth / 2.0, 0, pWidth / 2.0, pHeight / 2.0)
-                                                     InPixelBuffer:rgbPixelBuffer];// 右上角
-    NSArray<YYRGBPixelNode *> *LBNodes = [self getSquareSidePoints:sizePoint
+                                                     InPixelBuffer:yuvPixelBuffer];// 右上角
+    NSArray<YYPixelNode *> *LBNodes = [self getSquareSidePoints:sizePoint
                                                     PointsWithRect:CGRectMake(0, pHeight / 2.0, pWidth / 2.0, pHeight / 2.0)
-                                                     InPixelBuffer:rgbPixelBuffer]; // 左下角
-    NSArray<YYRGBPixelNode *> *RBNodes = [self getSquareSidePoints:sizePoint
+                                                     InPixelBuffer:yuvPixelBuffer]; // 左下角
+    NSArray<YYPixelNode *> *RBNodes = [self getSquareSidePoints:sizePoint
                                                     PointsWithRect:CGRectMake(pWidth / 2.0, pHeight / 2.0, pWidth / 2.0, pHeight / 2.0)
-                                                     InPixelBuffer:rgbPixelBuffer];// 右下角
+                                                     InPixelBuffer:yuvPixelBuffer];// 右下角
     
     BOOL LTIsColor = isColorRegion(LTNodes);
     BOOL RTIsColor = isColorRegion(RTNodes);
@@ -142,10 +137,6 @@
         NSLog(@"颜色区域分布不规则的MP4");
     }
     
-    CFRelease(rgbPixelBuffer);
-    rgbPixelBuffer = NULL;
-    
-    CVPixelBufferUnlockBaseAddress(rgbPixelBuffer, 0);
     CVPixelBufferUnlockBaseAddress(yuvPixelBuffer, 0);
     return result;
 }
@@ -155,24 +146,27 @@
                   PointsWithRect:(CGRect)rect
           InPixelBuffer:(CVPixelBufferRef)pixelBuffer
 {
-    void (^block)(YYRGBPixelNode *) = ^(YYRGBPixelNode *node) {
-        CVPixelBufferLockBaseAddress(pixelBuffer, 0);
-        unsigned char *baseAddress = CVPixelBufferGetBaseAddress(pixelBuffer);
+    void (^block)(YYPixelNode *) = ^(YYPixelNode *node) {
+        // 获取Y、UV平面的数据和行宽
+        uint8_t *yPlane = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 0);
+        uint8_t *uvPlane = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 1);
+        size_t yPitch = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 0);
+        size_t uvPitch = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 1);
         
-        size_t bytesPerRow = CVPixelBufferGetBytesPerRow(pixelBuffer);
-        size_t index = node.point.y * bytesPerRow + node.point.x*4;
+        // 计算当前像素的Y、U、V分量
+        unsigned long yIdx = node.point.y * yPitch + node.point.x;
+        unsigned long uvIdx = ((int)node.point.y >> 1) * uvPitch + (((int)node.point.x >> 1) << 1);
         
-        int b = *(baseAddress+index);
-        int g = *(baseAddress+index+1);
-        int r = *(baseAddress+index+2);
-        BOOL isGray = isGrayPixelWith(r, g, b);
+        int y = yPlane[yIdx];
+        int u = uvPlane[uvIdx];
+        int v = uvPlane[uvIdx + 1];
         
-        node.r = r;
-        node.g = g;
-        node.b = b;
+        BOOL isGray = isGrayPixelWith(u, v);
+        
+        node.y = y;
+        node.u = u;
+        node.v = v;
         node.isColorNode = !isGray;
-        
-        CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
     };
     
     NSMutableArray *result = @[].mutableCopy;
@@ -185,7 +179,7 @@
             
             for (int row = 0; row < pointNum; row++) {
                 int x = rect.origin.x + (row + 1) * xStep;
-                YYRGBPixelNode *node = [YYRGBPixelNode new];
+                YYPixelNode *node = [YYPixelNode new];
                 node.point = CGPointMake(x, y);
                 
                 block(node);
@@ -199,63 +193,6 @@
 }
 
 #pragma mark - get something
-- (CVPixelBufferRef)getRGBPixelBufferFromYUVPixelBuffer:(CVPixelBufferRef)yuvPx
-{
-    CVPixelBufferLockBaseAddress(yuvPx, 0);
-    //     将YUV图像转换为CIImage
-    CIImage *ciImage = [CIImage imageWithCVPixelBuffer:yuvPx options:nil];
-    // 将YUV图像转换为RGBA图像
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    CGImageRef rgbaImage = [self.ciContext createCGImage:ciImage fromRect:ciImage.extent format:kCIFormatBGRA8 colorSpace:colorSpace];
-    CGColorSpaceRelease(colorSpace);
-
-    CVPixelBufferRef rgbPixelBuffer = [self getCVPixelBufferFromImage:[UIImage imageWithCGImage:rgbaImage]];
-    
-    CFRelease(rgbaImage);
-    rgbaImage = NULL;
-    
-    CVPixelBufferUnlockBaseAddress(yuvPx, 0);
-    return rgbPixelBuffer;
-}
-
-- (CVPixelBufferRef)getCVPixelBufferFromImage:(UIImage *)img
-{
-    CGSize size = img.size;
-    CGImageRef image = [img CGImage];
-    
-    BOOL hasAlpha = CGImageRefContainsAlpha(image);
-    CFDictionaryRef empty = CFDictionaryCreate(kCFAllocatorDefault, NULL, NULL, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
-    
-    NSDictionary *options = [NSDictionary dictionaryWithObjectsAndKeys:
-                             [NSNumber numberWithBool:YES], kCVPixelBufferCGImageCompatibilityKey,
-                             [NSNumber numberWithBool:YES], kCVPixelBufferCGBitmapContextCompatibilityKey,
-                             empty, kCVPixelBufferIOSurfacePropertiesKey,
-                             nil];
-    CVPixelBufferRef pxbuffer = NULL;
-    CVReturn status = CVPixelBufferCreate(kCFAllocatorDefault, size.width, size.height, inputPixelFormat(), (__bridge CFDictionaryRef) options, &pxbuffer);
-    
-    NSParameterAssert(status == kCVReturnSuccess && pxbuffer != NULL);
-    
-    CVPixelBufferLockBaseAddress(pxbuffer, 0);
-    void *pxdata = CVPixelBufferGetBaseAddress(pxbuffer);
-    NSParameterAssert(pxdata != NULL);
-    
-    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
-    
-    uint32_t bitmapInfo = bitmapInfoWithPixelFormatType(inputPixelFormat(), (bool)hasAlpha);
-    
-    CGContextRef context = CGBitmapContextCreate(pxdata, size.width, size.height, 8, CVPixelBufferGetBytesPerRow(pxbuffer), rgbColorSpace, bitmapInfo);
-    NSParameterAssert(context);
-    
-    CGContextDrawImage(context, CGRectMake(0, 0, CGImageGetWidth(image), CGImageGetHeight(image)), image);
-    CVPixelBufferUnlockBaseAddress(pxbuffer, 0);
-    
-    CGColorSpaceRelease(rgbColorSpace);
-    CGContextRelease(context);
-    
-    return pxbuffer;
-}
-
 - (CMSampleBufferRef)getNextSampleBufferRefWithStep:(NSInteger)step {
     CMSampleBufferRef sampleBufferRef = NULL;
     if (_reader.status == AVAssetReaderStatusReading) {
@@ -272,61 +209,19 @@
 }
 
 
-#pragma mark - lazy load
-- (CIContext *)ciContext
-{
-    if (!_ciContext) {
-        _ciContext = [[CIContext alloc] init];
+static BOOL isGrayPixelWith(int u, int v) {
+    int tolerance = 10;
+    // 允许一定容差，判断该像素是否为灰度像素
+    if (abs(u - 128) <= tolerance && abs(v - 128) <= tolerance) {
+        return YES;
     }
-    return _ciContext;
-}
-
-#pragma mark - static
-static OSType inputPixelFormat(void) {
-    return kCVPixelFormatType_32BGRA;
-}
-
-static uint32_t bitmapInfoWithPixelFormatType(OSType inputPixelFormat, bool hasAlpha) {
     
-    if (inputPixelFormat == kCVPixelFormatType_32BGRA) {
-        uint32_t bitmapInfo = kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Host;
-        if (!hasAlpha) {
-            bitmapInfo = kCGImageAlphaNoneSkipFirst | kCGBitmapByteOrder32Host;
-        }
-        return bitmapInfo;
-    }else if (inputPixelFormat == kCVPixelFormatType_32ARGB) {
-        uint32_t bitmapInfo = kCGImageAlphaPremultipliedFirst | kCGBitmapByteOrder32Big;
-        return bitmapInfo;
-    }else{
-        NSLog(@"不支持此格式");
-        return 0;
-    }
+    return NO;
 }
 
-// alpha的判断
-static BOOL CGImageRefContainsAlpha(CGImageRef imageRef) {
-    if (!imageRef) {
-        return NO;
-    }
-    CGImageAlphaInfo alphaInfo = CGImageGetAlphaInfo(imageRef);
-    BOOL hasAlpha = !(alphaInfo == kCGImageAlphaNone ||
-                      alphaInfo == kCGImageAlphaNoneSkipFirst ||
-                      alphaInfo == kCGImageAlphaNoneSkipLast);
-    return hasAlpha;
-}
-
-static BOOL isGrayPixelWith(int r, int g, int b) {
-    if (abs(r - g) > 10 || abs(g - b) > 10 || abs(b - r) > 10) {
-        // 差异较大，不是灰度图像
-        return NO;
-    }
-    // 差异较小，是灰度图像
-    return YES;
-}
-
-static BOOL isColorRegion(NSArray<YYRGBPixelNode *>* nodes) {
+static BOOL isColorRegion(NSArray<YYPixelNode *>* nodes) {
     __block BOOL isColor = NO;
-    [nodes enumerateObjectsUsingBlock:^(YYRGBPixelNode *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+    [nodes enumerateObjectsUsingBlock:^(YYPixelNode *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
         if (obj.isColorNode) {
             isColor = obj.isColorNode;
         
